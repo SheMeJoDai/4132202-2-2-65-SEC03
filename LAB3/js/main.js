75238858a187f4d86f7c89c631206a3cf5028905
SheMeJoDai <640112418092@bru.ac.th>
2022-11-24 15:56:50 +0700

    LAB3

---
+++ LAB3/js/main.js
@@ -0,0 +1,43 @@
+
+    // alert("Hello js!");
+    var num = 10;//global
+    let name = "Kao";//scope
+    const arr = [10, 20, "Hi"];
+    let obj = { name: "Dang", age: 20 };
+    const objmix = {
+        data: [
+            { name: "Dum", gpa: 3.51 },
+            { name: "Dong", age: 18, gpa: 3.12 }
+        ]
+    };
+
+    console.log(num);
+    console.log(arr[2]);
+    console.log(objmix.data[1].age);
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    let total = add(3, 5);
+
+    document.getElementById("div1").innerHTML = name;
+
+    $(function () {
+        // alert("Hello jQuery...");
+
+        $("#btn1").click(function () {
+            $("#div1").text("BRU");
+        });
+
+        $("#btn2").bind("click",()=>{
+            $(".in1").val("BRU");
+        });
+
+        $("#btn3").click(()=>{
+            $(".in1:even").toggleClass("red");
+        });
+
+        $("#div_form").load("./pages/form.html");
+
+    });//jQuery Handle
